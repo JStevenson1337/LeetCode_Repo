c607face7b960fd523dedbd083fc25e50fb510f8
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2023-09-18 08:24:32 -0500

    add 1

---
+++ 1337._The_K_Weakest_Rows_in_a_Matrix/typeCast.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+typedef struct {
+    int id;
+    char name[50];
+} Row;
+
+void processRow(void *a) {
+    Row *rowA = (Row *)a;
+    // Access the fields of the Row struct
+    printf("ID: %d\n", rowA->id);
+    printf("Name: %s\n", rowA->name);
+}
+
+int main() {
+    Row row;
+    row.id = 1;
+    strcpy(row.name, "John Doe");
+
+    // Pass the address of the row struct to the processRow function
+    processRow(&row);
+
+    return 0;
+}
